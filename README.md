b88ed2aa6d6513b143adc51df5768c09822a8da6
Chris McDearman <mcdearmanchris@gmail.com>
2023-03-31 06:03:23 -0500

    alter repl to support multilines

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # typed-lambda-calculus
+
+A simple interpreter for a typed lambda calculus with some additional features like `let` bindings and infix operations.
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use std::collections::HashMap;
 use std::fmt::{Debug, Display};
 use std::fs;
 use std::fs::File;
+use std::io;
+use std::io::Write;
 use std::{cell::RefCell, rc::Rc};
 
 // =====================================================================
@@ -86,7 +88,7 @@ pub enum Token {
     LParen,
     #[token(")")]
     RParen,
-    #[regex(r"[ \t]", logos::skip)]
+    #[regex(r"[ \t\n\r]", logos::skip)]
     Whitespace,
     #[error]
     Err,
@@ -434,49 +436,89 @@ impl ReplHelper {
 pub fn repl() {
     let mut rl = Editor::new().expect("failed to create editor");
     rl.set_helper(Some(ReplHelper::new()));
-    let env = Rc::new(RefCell::new(Env::new()));
 
     println!("Welcome to the Lust REPL!");
-    loop {
-        match rl.readline("> ") {
-            Ok(line) => {
-                // let tokens: Vec<Token> = Token::lexer(&line).clone().spanned().collect();
-                // println!("Tokens: {:?}", tokens);
-
-                let lex = Token::lexer(&line)
+    print!("> ");
+    io::stdout().flush().expect("failed to flush stdout");
+    let mut src = String::new();
+    let mut carry = String::new();
+    let env = Rc::new(RefCell::new(Env::new()));
+    'outer: loop {
+        std::io::stdin()
+            .read_line(&mut src)
+            .expect("failed to read line");
+        for expr_str in src.trim_end().split_inclusive(";;") {
+            if expr_str.ends_with(";;") {
+                let expr_str = carry.clone() + expr_str.trim_end_matches(";;");
+                let lex = Token::lexer(&expr_str)
                     .spanned()
                     .map(|(tok, span)| (tok, SimpleSpan::from(span)));
-
                 let tok_stream =
-                    Stream::from_iter(lex).spanned(SimpleSpan::from(line.len()..line.len()));
-                let ast = parser().parse(tok_stream).into_result();
-                match ast {
+                    Stream::from_iter(lex).spanned(SimpleSpan::from(src.len()..src.len()));
+                match parser().parse(tok_stream).into_result() {
                     Ok(ast) => match eval(env.clone(), &ast) {
-                        Ok(result) => {
-                            // println!("AST: {:?}", ast.clone());
-                            println!("{}", result)
-                        }
-                        Err(err) => eprintln!("RuntimeError: {}", err),
+                        Ok(val) => println!("{}", val),
+                        Err(e) => println!("Error: {}", e),
                     },
-                    Err(parse_errs) => parse_errs.into_iter().for_each(|err| {
-                        eprintln!("Parse error: {}", err);
-                    }),
-                };
-            }
-            Err(ReadlineError::Interrupted) => {
-                println!("Ctrl-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("Ctrl-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        carry.clear();
+                        continue 'outer;
+                    }
+                }
+                carry.clear();
+            } else {
+                carry.push_str(expr_str);
+                src.clear();
+                print!("- ");
+                io::stdout().flush().expect("failed to flush stdout");
+                continue 'outer;
             }
         }
+        src.clear();
+        print!("\n> ");
+        io::stdout().flush().expect("failed to flush stdout");
     }
+    // loop {
+    //     match rl.readline("> ") {
+    //         Ok(line) => {
+    //             // let tokens: Vec<Token> = Token::lexer(&line).clone().spanned().collect();
+    //             // println!("Tokens: {:?}", tokens);
+
+    //             let lex = Token::lexer(&line)
+    //                 .spanned()
+    //                 .map(|(tok, span)| (tok, SimpleSpan::from(span)));
+
+    //             let tok_stream =
+    //                 Stream::from_iter(lex).spanned(SimpleSpan::from(line.len()..line.len()));
+    //             let ast = parser().parse(tok_stream).into_result();
+    //             match ast {
+    //                 Ok(ast) => match eval(env.clone(), &ast) {
+    //                     Ok(result) => {
+    //                         // println!("AST: {:?}", ast.clone());
+    //                         println!("{}", result)
+    //                     }
+    //                     Err(err) => eprintln!("RuntimeError: {}", err),
+    //                 },
+    //                 Err(parse_errs) => parse_errs.into_iter().for_each(|err| {
+    //                     eprintln!("Parse error: {}", err);
+    //                 }),
+    //             };
+    //         }
+    //         Err(ReadlineError::Interrupted) => {
+    //             println!("Ctrl-C");
+    //             break;
+    //         }
+    //         Err(ReadlineError::Eof) => {
+    //             println!("Ctrl-D");
+    //             break;
+    //         }
+    //         Err(err) => {
+    //             println!("Error: {:?}", err);
+    //             break;
+    //         }
+    //     }
+    // }
 }
 
 // =====================================================================
@@ -484,12 +526,17 @@ pub fn repl() {
 // =====================================================================
 
 fn main() {
-    let src = fs::read_to_string("examples/multiline.tlc").expect("failed to read file");
-    let lex = Token::lexer(&src)
-        .spanned()
-        .map(|(tok, span)| (tok, SimpleSpan::from(span)));
-    let tok_stream = Stream::from_iter(lex).spanned(SimpleSpan::from(src.len()..src.len()));
-    let ast = parser().parse(tok_stream).into_result();
-    println!("{:?}", ast);
-    // repl();
+    // let src = fs::read_to_string("examples/multiline.tlc").expect("failed to read file");
+    // let lex = Token::lexer(&src)
+    //     .spanned()
+    //     .map(|(tok, span)| (tok, SimpleSpan::from(span)));
+    // let tok_stream = Stream::from_iter(lex).spanned(SimpleSpan::from(src.len()..src.len()));
+    // let ast = parser()
+    //     .parse(tok_stream)
+    //     .into_result()
+    //     .expect("failed to parse");
+    // println!("{:?}", ast);
+    // let val = eval(Rc::new(RefCell::new(Env::new())), &ast).expect("failed to eval");
+    // println!("{:?}", val);
+    repl();
 }
