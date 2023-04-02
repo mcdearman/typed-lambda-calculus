eadf664a5aed16c63df7a154ff96b084c9f96604
mcdearman <mcdearmanchris@gmail.com>
2023-04-02 14:25:10 -0500

    add tests

--- src/tests.rs
+++ src/tests.rs
@@ -19,7 +19,35 @@ fn test_parse_let() {
 
 #[test]
 fn test_parse_multiline_let() {
-    let src = "let x = \n5 in x";
+    let src = "let x =\n5 in x";
+    let lex = Token::lexer(&src)
+        .spanned()
+        .map(|(tok, span)| (tok, SimpleSpan::from(span)));
+    let tok_stream = Stream::from_iter(lex).spanned(SimpleSpan::from(src.len()..src.len()));
+    let ast = parser()
+        .parse(tok_stream)
+        .into_result()
+        .expect("failed to parse");
+    insta::assert_debug_snapshot!(ast);
+}
+
+#[test]
+fn test_parse_apply() {
+    let src = "let f = \\x -> x in f 5";
+    let lex = Token::lexer(&src)
+        .spanned()
+        .map(|(tok, span)| (tok, SimpleSpan::from(span)));
+    let tok_stream = Stream::from_iter(lex).spanned(SimpleSpan::from(src.len()..src.len()));
+    let ast = parser()
+        .parse(tok_stream)
+        .into_result()
+        .expect("failed to parse");
+    insta::assert_debug_snapshot!(ast);
+}
+
+#[test]
+fn test_parse_multiline_apply() {
+    let src = "let f =\n\\x -> x in f 5";
     let lex = Token::lexer(&src)
         .spanned()
         .map(|(tok, span)| (tok, SimpleSpan::from(span)));
